2a714c15b9c9b7ad48a906c5725638c9d6e9a445
Abhijit Mohan Lamb <106537258+abhijitlamb03@users.noreply.github.com>
2022-12-12 15:14:41 +0530

    s
    s

--- js/app.js
+++ js/app.js
@@ -6,72 +6,6 @@ const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
     
-// code by me///////
-// // search movies
-
-
-
-
-// const moiveBox = document.querySelector("#movie-box")
-// //init call
-// const getMovies = async (api) => {
-
-//     const response = await fetch(api)
-//     const data = await response.json()
-//     // console.log(data)
-//      showMovies(data.results)
-
-
-
-
-// }
-
-
-// const showMovies = (data) => {
-
-//     moiveBox.innerHTML = "";
-//     //empty the movie box
-
-//     data.forEach((item) => {
-//         // console.log(item)
-//         const box = document.createElement("div")
-//                     box.classList.add("box")
-//                     box.innerHTML = `
-//                         <img src="${IMGPATH+item.poster_path}" alt="" />
-//                         <div class="overlay">
-//                             <div class="title"> 
-//                                 <h2>${item.original_title}</h2>
-//                                 <span>${item.vote_average} <span>
-//                             </div>
-//                             <h3>Overview:</h3>
-//                             <p> 
-//                                 ${item.overview}
-//                             </p>
-//                          </div>
-//                     `
-//                     moiveBox.appendChild(box)
-
-
-//     });
-
-
-// }
-// document.querySelector("#search").addEventListener(
-
-//     "keyup",
-//     function (event) {
-
-//         if(event.target.value != null){
-//             getMovies(SEARCHAPI+event.target.value)
-
-//         }else{
-
-//             getMovies(APIURL)
-//         }
-        
-//     }
-// )
-// getMovies(APIURL);
 
 
 const moiveBox = document.querySelector("#movie-box")
@@ -88,15 +22,7 @@ const showMovies = (data) => {
     data.results.forEach(
         (result) => {
             const imagePath = result.poster_path === null ? "img/image-missing.png" : IMGPATH + result.poster_path;
-            // const box = `
-            // <div class="box">
-            //     <img src="${IMGPATH+result}" alt="" />
-            //     <div class="overlay">
-            //         <h2>Overview:</h2>
-            //         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis iste doloribus quam voluptatum, illum unde nostrum dignissimos, mollitia, sapiente porro natus neque cupiditate distinctio quod possimus aliquid reiciendis vel. Soluta?
-            //     </div>
-            // </div>
-            // `
+            
             const box = document.createElement("div")
             box.classList.add("box")
             box.innerHTML = `
